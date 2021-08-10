c89f574116e5b585d80e6b3592409aa36940e568
Holger Huo <admin@holger.net.cn>
2021-08-10 23:06:52 +0800

    feat: show stats change

--- README.md
+++ README.md
@@ -11,4 +11,5 @@ client_secret=""
 access_token=""
 instance="https://mast.dragon-fly.club"
 visibility="unlisted"
+show_change=True
 ```
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from mastodon import Mastodon
 import config
+import json
+import os
 import sys
 from datetime import datetime
 
@@ -25,11 +27,36 @@ text = """
 期待与各位Islanders一同创造的明天~
 """
 
-text = text.replace("%%version%%", instance.version)
-text = text.replace("%%usercount%%", str(instance.stats.user_count))
-text = text.replace("%%statuscount%%", str(instance.stats.status_count))
-text = text.replace("%%domaincount%%", str(instance.stats.domain_count))
-text = text.replace("%%timestamp%%", time)
+if not config.show_change or not os.path.exists('db.json'):
+  text = text.replace("%%version%%", instance.version)
+  text = text.replace("%%usercount%%", str(instance.stats.user_count))
+  text = text.replace("%%statuscount%%", str(instance.stats.status_count))
+  text = text.replace("%%domaincount%%", str(instance.stats.domain_count))
+  text = text.replace("%%timestamp%%", time)
+else:
+  
+  f = open('db.json', 'r')
+  db = json.loads(f.read())
+  f.close()
+  
+  domain_change = instance.stats.domain_count-db["domain_count"]
+  status_change = instance.stats.status_count-db["status_count"]
+  user_change = instance.stats.user_count-db["user_count"]
+  
+  if instance.version == db["version"]:
+    text = text.replace("%%version%%", instance.version)
+  else:
+    text = text.replace("%%version%%", db["version"] + "➡" + instance.version)
+  text = text.replace("%%usercount%%", str(instance.stats.user_count) + '(%+-d)'%user_change)
+  text = text.replace("%%statuscount%%", str(instance.stats.status_count) + '(%+-d)'%status_change)
+  text = text.replace("%%domaincount%%", str(instance.stats.domain_count) + '(%+-d)'%domain_change)
+  text = text.replace("%%timestamp%%", time)
+
+db={'version':instance.version,'user_count':instance.stats.user_count,'status_count':instance.stats.status_count,'domain_count': instance.stats.domain_count}
+db = json.dumps(db)
+f = open('db.json', 'w')
+f.write(db)
+f.close()
 
 dryrun = False
 try:
