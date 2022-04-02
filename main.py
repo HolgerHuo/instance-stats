bf1360ac2ab1b266d7deacfcadbc57c6b681fd50
Holger Huo <holgerhuo@outlook.com>
2022-04-02 18:04:00 +0800

    update: do not require source instance key for parsing info

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from mastodon import Mastodon
+import requests
 import config
 import json
 import os
@@ -7,23 +8,21 @@ import sys
 from datetime import datetime
 
 client = Mastodon(
-  client_id=config.client_id,
-  client_secret=config.client_secret,
   access_token=config.access_token,
-  api_base_url=config.instance
+  api_base_url=config.toot_instance
 )
-me = client.account_verify_credentials()
-
-instance = client.instance()
-time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-
 text = config.text
 
+time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
+instance = requests.get(config.source_instance+'/api/v1/instance').json()
+
+
 if not config.show_change or not os.path.exists('db.json'):
-  text = text.replace("%%version%%", instance.version)
-  text = text.replace("%%usercount%%", str(instance.stats.user_count))
-  text = text.replace("%%statuscount%%", str(instance.stats.status_count))
-  text = text.replace("%%domaincount%%", str(instance.stats.domain_count))
+  text = text.replace("%%version%%", instance['version'])
+  text = text.replace("%%usercount%%", str(instance['stats']['user_count']))
+  text = text.replace("%%statuscount%%", str(instance['stats']['status_count']))
+  text = text.replace("%%domaincount%%", str(instance['stats']['domain_count']))
   text = text.replace("%%timestamp%%", time)
 else:
   
@@ -31,20 +30,20 @@ else:
   db = json.loads(f.read())
   f.close()
   
-  domain_change = instance.stats.domain_count-db["domain_count"]
-  status_change = instance.stats.status_count-db["status_count"]
-  user_change = instance.stats.user_count-db["user_count"]
+  domain_change = instance['stats']['domain_count']-db["domain_count"]
+  status_change = instance['stats']['status_count']-db["status_count"]
+  user_change = instance['stats']['user_count']-db["user_count"]
   
-  if instance.version == db["version"]:
-    text = text.replace("%%version%%", instance.version)
+  if instance['version'] == db["version"]:
+    text = text.replace("%%version%%", instance['version'])
   else:
-    text = text.replace("%%version%%", db["version"] + "➡" + instance.version)
-  text = text.replace("%%usercount%%", str(instance.stats.user_count) + '(%+-d)'%user_change)
-  text = text.replace("%%statuscount%%", str(instance.stats.status_count) + '(%+-d)'%status_change)
-  text = text.replace("%%domaincount%%", str(instance.stats.domain_count) + '(%+-d)'%domain_change)
+    text = text.replace("%%version%%", db["version"] + "➡" + instance['version'])
+  text = text.replace("%%usercount%%", str(instance['stats']['user_count']) + '(%+-d)'%user_change)
+  text = text.replace("%%statuscount%%", str(instance['stats']['status_count']) + '(%+-d)'%status_change)
+  text = text.replace("%%domaincount%%", str(instance['stats']['domain_count']) + '(%+-d)'%domain_change)
   text = text.replace("%%timestamp%%", time)
 
-db={'version':instance.version,'user_count':instance.stats.user_count,'status_count':instance.stats.status_count,'domain_count': instance.stats.domain_count}
+db={'version':instance['version'],'user_count':instance['stats']['user_count'],'status_count':instance['stats']['status_count'],'domain_count': instance['stats']['domain_count']}
 db = json.dumps(db)
 f = open('db.json', 'w')
 f.write(db)
